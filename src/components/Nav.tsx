3e16c9fae708531674cca4613c0a32a7bf60cbd4
UjjwalAglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2024-06-13 22:11:33 +0530

    New Front End

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,43 +3,34 @@ import { TonConnectButton } from '@tonconnect/ui-react'
 
 function Nav() {
   return (
-   <>
-<div className="fixed z-10 backdrop-blur-sm">
-  <section className="relative mx-auto">
-      
-    <nav className="flex justify-between text-white w-screen max-sm:px-0 px-24">
-      <div className="px-5 xl:px-12 py-6 flex max-sm:gap-10 w-full items-center">
-        <a className="text-3xl max-sm:text-base font-bold font-heading">
-          Ignitus Networks
-        </a>
-       
-        <ul className="md:flex max-sm:flex max-sm:px-0 px-4 mx-auto max-sm:text-sm font-semibold font-heading max-sm:space-x-6 space-x-12">
-         
-        <Link className='no-underline text-gray-200'  to="/">
-          <li>Home</li>   </Link>  
-          
-          <Link className='no-underline text-gray-200' to="/all-nft">
-          <li>All NFT</li>   </Link>
-        
-          <Link className='no-underline text-gray-200' to="/create"> 
-          <li>Mint NFT</li>   </Link>  
-        </ul>
-       
-        <div className=" hidden xl:flex items-center space-x-5">
-         
-        
-        <TonConnectButton></TonConnectButton>
-        </div>
+    <>
+      <div className="fixed z-10 backdrop-blur-sm w-full">
+        <section className="relative mx-auto">
+          <nav className="flex justify-between text-white w-full px-4 md:px-24">
+            <div className="px-5 xl:px-12 py-6 flex gap-10 w-full items-center justify-between">
+              <a className="text-3xl font-bold font-heading">
+                Ignitus Networks
+              </a>
+              <ul className="flex flex-wrap px-0 mx-auto text-sm md:text-base font-semibold font-heading space-x-6 md:space-x-12">
+                <Link className='no-underline text-gray-200' to="/">
+                  <li>Home</li>
+                </Link>
+                <Link className='no-underline text-gray-200' to="/all-nft">
+                  <li>All NFT</li>
+                </Link>
+                <Link className='no-underline text-gray-200' to="/create">
+                  <li>Mint NFT</li>
+                </Link>
+              </ul>
+              <div className="flex items-center space-x-5">
+                <TonConnectButton />
+              </div>
+            </div>
+          </nav>
+        </section>
       </div>
-    
 
-    </nav>
-    
-  </section>
-</div>
-
-
-   </>
+    </>
   )
 }
 
